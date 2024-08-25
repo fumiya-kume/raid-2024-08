eb169b0497783c728f312c0f32b2bab233e77e56
fumiya-kume <fumiya.kume@hotmail.com>
2024-08-26 00:20:31 +0900

    Add navigation for game view from standby screen

--- iOS/fun app/fun app/standby/StandByView.swift
+++ iOS/fun app/fun app/standby/StandByView.swift
@@ -13,11 +13,7 @@ struct StandByView: View {
     var body: some View {
         VStack {
             Text("現在　\(viewModel.standByUserCount)人待機中...")
-            Button {
-                print("tap start button")
-            } label: {
-                Text("Start!")
-            }
+            NavigationLink("Start!", destination: GameView())
         }
     }
 }
