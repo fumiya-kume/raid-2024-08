52180d7afc7366715544287465fb0b2fcf8854cf
fumiya-kume <fumiya.kume@hotmail.com>
2024-08-25 17:50:55 +0900

    Add some data fetch method

--- iOS/fun app/fun app/Repository.swift
+++ iOS/fun app/fun app/Repository.swift
@@ -14,6 +14,7 @@ class Repository {
     
     func countUser() async -> Int {
         do {
+            let result = try await firestore.collection("sessions").getDocuments()
             let documentsRef = try await firestore.collection("users").getDocuments()
             let count = documentsRef.count
             print(count)
@@ -22,19 +23,66 @@ class Repository {
             return -1
         }
     }
+    
+    func fetchStartTime() async -> Date? {
+        do {
+            let sessionsRef = try await firestore.collection("sessions").getDocuments()
+            let session = try sessionsRef.documents.map{
+                try $0.data(as: Session.self)
+            }.first
+            return session?.startTime
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func fetchIsEnd() async -> Bool? {
+        do {
+            let sessionsRef = try await firestore.collection("sessions").getDocuments()
+            let session = try sessionsRef.documents.map{
+                try $0.data(as: Session.self)
+            }.first
+            return session?.isEnded
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func fetchSession() async -> Session? {
+        do {
+            let sessionsRef = try await firestore.collection("sessions").getDocuments()
+            let session = try sessionsRef.documents.map{
+                try $0.data(as: Session.self)
+            }.first
+            print(session)
+            return nil
+        } catch {
+            print(error)
+            return nil
+        }
+    }
 }
 
-struct Session {
+
+struct Session: Codable {
     let startTime: Date
-    let user: [User]
+    let users: [DocumentReference]
     let isEnded: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case startTime = "start_time"
+        case isEnded = "is_end"
+        case users = "users"
+    }
 }
 
-struct User {
+struct User:Codable {
     let items: [Item]
 }
 
-struct Item {
+struct Item:Codable {
     let name: String
     let price: Int
 }
