ab5f85f46251c7f03ecbefde1a4701416bbf3334
fumiya-kume <fumiya.kume@hotmail.com>
2024-08-25 17:14:35 +0900

    Fix ContentView for removing the SwiftData

--- iOS/fun app/fun app/ContentView.swift
+++ iOS/fun app/fun app/ContentView.swift
@@ -9,34 +9,15 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-    
     @StateObject private var viewModel = ContentViewModel()
-
+    
     var body: some View {
         Button(viewModel.buttonText) {
             viewModel.updateButtonName()
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
