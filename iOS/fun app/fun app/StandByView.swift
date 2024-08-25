b2c94854a1e29fb3f68fe7a0c0fe7edc2779acba
fumiya-kume <fumiya.kume@hotmail.com>
2024-08-25 19:28:52 +0900

    improve thread handling in Stand by view

--- iOS/fun app/fun app/StandByView.swift
+++ iOS/fun app/fun app/StandByView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct StandByView: View {
-    @StateObject private var viewModel = StandByViewModel()
+    @ObservedObject private var viewModel = StandByViewModel()
 
     var body: some View {
         VStack {
-            Text("現在　\(viewModel.fetchStandByUserCount())人待機中...")
+            Text("現在　\(viewModel.standByUserCount)人待機中...")
             Button {
                 print("tap start button")
             } label: {
