d87124fa48a27e47cae1e5068bfb1b59bb285d05
fumiya-kume <fumiya.kume@hotmail.com>
2024-08-31 12:59:38 +0900

    Remove Firebase

--- iOS/fun app/fun app/fun_appApp.swift
+++ iOS/fun app/fun app/fun_appApp.swift
@@ -7,12 +7,10 @@
 
 import SwiftUI
 import SwiftData
-import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
         
         return true
     }
